8fd0b744a6d4ea3642a1528ccfe8b72175c376d7
albertng <albertng412@gmail.com>
2021-04-06 20:56:55 +0800

    update dashboard be

---
+++ admin-dashboard-angular/be/main.py
@@ -0,0 +1,117 @@
+from flask_cors import cross_origin
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/getchartdata", methods=['GET', 'POST'])
+@cross_origin()
+def submit():
+    if request.method == 'POST':
+        #json_data = request.json
+        request_data = request.json
+
+        return {
+            "Linechartyearshowmonth": [
+                [
+                    210
+                ],
+                [
+                    79
+                ],
+                [
+                    170
+                ],
+                [
+                    130
+                ],
+                [
+                    242
+                ],
+                [
+                    201
+                ],
+                [
+                    200
+                ],
+                [
+                    159
+                ],
+                [
+                    236
+                ],
+                [
+                    30
+                ],
+                [
+                    264
+                ],
+                [
+                    348
+                ]
+            ],
+            "Year": [
+                [
+                    "Lighting Music     ",
+                    22
+                ],
+                [
+                    "Football          ",
+                    12
+                ],
+                [
+                    "Marvel        ",
+                    83
+                ],
+                [
+                    "Logic and thinking           ",
+                    17
+                ],
+                [
+                    "Budda       ",
+                    51
+                ],
+                [
+                    "Deep learning           ",
+                    260
+                ]
+            ],
+            "Month": [
+                [
+                    "Cantoneses       ",
+                    1513
+                ],
+                [
+                    "Music Festival      ",
+                    104
+                ],
+                [
+                    "CSCI3100 Software Engineering  ",
+                    208
+                ]
+            ],
+            "typeofitem": [
+                [
+                    "Sport     ",
+                    166
+                ],
+                [
+                    "Music      ",
+                    265
+                ],
+                [
+                    "technolgoy   ",
+                    80
+                ],
+                [
+                    "Science   ",
+                    80
+                ]
+            ]
+        }
+    return 'Cant Retrieve'
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
